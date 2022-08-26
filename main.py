c4af1c4048ecb594d6585a4c0206e5398503f6b1
Valarrr1 <108575570+Valarrr1@users.noreply.github.com>
2022-08-26 17:31:34 +0300

    V0.1.0

---
+++ main.py
@@ -0,0 +1,27 @@
+
+import platform # importing class
+
+#computer network name
+
+print(f"Computer network name: {platform.node()}")#buradaki f ne işe yarıyor anlayamadım.
+
+#machine type
+print(f"Machine type:  {platform.machine()}")
+
+#proccesor type
+print(f"Proccesors type: {platform.processor()}")
+
+#platform type
+print(f"Platform type: {platform.platform()}")
+
+#operating system
+print(f"OS : {platform.system()}")
+
+#operating system relase
+print(f"OS release: {platform.release()}")
+
+#OS version
+
+print(f"OS version : {platform.version()}")
+
+
